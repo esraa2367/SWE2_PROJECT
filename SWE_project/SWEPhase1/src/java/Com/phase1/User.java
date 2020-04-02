b7342e88d8724b946fa54b5052deb71a0319db5d
lubnahassan <36442977+lubnahassan@users.noreply.github.com>
2020-04-02 22:10:20 +0200

    Implementation is added

---
+++ SWE_project/SWEPhase1/src/java/Com/phase1/User.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Com.phase1;
+/**
+ *
+ * @author Lubna
+ */
+public class User
+{
+    public String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    /*public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String type;
+    */
+    public String password;
+    public String userName;
+    
+}
